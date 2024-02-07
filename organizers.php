ca9d8d8ee7fc6cd2efa2cfff630f9c27314d5e4a
xiaoZ857 <786187157@qq.com>
2024-02-07 12:03:14 +0800

    css test

--- organizers.php
+++ organizers.php
@@ -14,9 +14,21 @@ function Nav() {
 }
 </script>
 <div style="display: flex; justify-content: space-between; margin-top: 0%; margin-bottom: 5%;">
-	<div style="flex: 5;">
-		
+	
+	<div style="flex: 1;text-align: left;">
+        <button class="small-button" onclick="Nav()">Committees</button><br/>
+        <div id="navContainer" style="height: 0; overflow: hidden; transition: 0.5s;">
+            <a href="advisoryC.php">Advisory Committee</a>
+            <a href="organizers.php">Organizing Committee</a>
+            <a href="TPC.php">Technical Program Committee</a>
+            <a href="IFEC.php">Industry Forums & Exhibition Committee</a>
+            <a href="operationC.php">Operations Committee</a>
+            <a href="volunteer.php">Volunteer</a>
+            
+        </div>
+    </div>
 
+	<div style="flex: 5;">
 		<div class="flex flex-auto person-container">
 			<div class="pic headshot round" style="background:url(images/organizers/wuzhiqiang.png);background-size:100px 100px;"></div>
 			<div class="info-container">
@@ -172,18 +184,6 @@ function Nav() {
 
 	</div>
 
-    <div style="flex: 1;padding-left: 10px; text-align: left;">
-        <button class="small-button" onclick="Nav()">Committees</button><br/>
-        <div id="navContainer" style="height: 0; overflow: hidden; transition: 0.5s;">
-            <a href="advisoryC.php">Advisory Committee</a>
-            <a href="organizers.php">Organizing Committee</a>
-            <a href="TPC.php">Technical Program Committee</a>
-            <a href="IFEC.php">Industry Forums & Exhibition Committee</a>
-            <a href="operationC.php">Operations Committee</a>
-            <a href="volunteer.php">Volunteer</a>
-            
-        </div>
-    </div>
 
 
 
