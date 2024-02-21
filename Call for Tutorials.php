6768d9ccf7b9534f8e763cb5b52ac731c119754f
JiayoMa <AndyMa5@outlook.com>
2024-02-21 21:11:58 +0800

    一些拼写纠正

--- Call for Tutorials.php
+++ Call for Tutorials.php
@@ -23,7 +23,7 @@ function Nav() {
             <div class="spacer" style="height: 20px;"></div> <!-- 使用百分比单位 -->
             <a href="Call for Tutorials.php" style="margin-left: 2%;">Call for Tutorials</a> <!-- 使用百分比单位 -->
             <div class="spacer" style="height: 20px;"></div> <!-- 使用百分比单位 -->
-            <a href="Workshops.php" style="margin-left: 2%;">workshop chairs</a> <!-- 使用百分比单位 -->
+            <a href="Workshopchairs.php" style="margin-left: 2%;">workshop chairs</a> <!-- 使用百分比单位 -->
         </div>
     </div>
 </div>
