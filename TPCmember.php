fd60d350bd1047fe498aa8368d1f47c76f8469f8
JiayoMa <AndyMa5@outlook.com>
2024-03-01 16:05:46 +0800

    错误订正

--- TPCmember.php
+++ TPCmember.php
@@ -1,5 +1,5 @@
 <?php 
-$pageTitle='TPC member<'; //THIS MUST BE SET FOR EACH PAGE
+$pageTitle='TPC member'; //THIS MUST BE SET FOR EACH PAGE
 $stylesheet='css/organizers.css';
 require 'header-left-bar.php'; //THIS MUST BE INCLUDED FOR EACH PAGE
 ?>
